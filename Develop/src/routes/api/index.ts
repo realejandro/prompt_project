9745874b4f84981b17bd509a43ad742793a54e9b
realejandro <realejandrocf@gmail.com>
2025-04-21 21:00:05 -0500

    deleting develop folder

--- Develop/src/routes/api/index.ts
+++
@@ -1,7 +0,0 @@
-import summarizeRoutes from './summarizeRoutes.js';
-import express from 'express';
-const router = express.Router();
-
-router.use('/summarize', summarizeRoutes);
-
-export default router;
--- Develop/src/routes/api/summarizeRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getSummary } from '../../controllers/summarizeController.js';
-
-const router = express.Router();
-
-router.post('/', getSummary);
-
-export default router;
